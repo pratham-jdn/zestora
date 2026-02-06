6215a4f3b3b746f1ba22b63badb3782316759823
pratham-jdn <2003prathamkumar@gmail.com>
2026-02-06 23:11:48 +0530

    created category card

---
+++ frontend/src/category.js
@@ -0,0 +1,24 @@
+import image1 from "./assets/image1.jpg"
+import image2 from "./assets/image2.webp"
+import image3 from "./assets/image3.jpg"
+import image4 from "./assets/image4.avif"
+import image5 from "./assets/image5.jpg"
+import image6 from "./assets/image6.jpg"
+import image7 from "./assets/image7.jpg"
+import image9 from "./assets/image9.jpg"
+import image10 from "./assets/image10.jpg"
+import image11 from "./assets/image11.jpg"
+
+
+export const categories = [
+    {"category": "Snacks", "image":image1},
+    {"category": "Main Course", "image":image2},
+    {"category": "Desserts", "image":image3},
+    {"category": "Pizza", "image":image4},
+    {"category": "Burger", "image":image5},
+    {"category": "Appetizers", "image":image6},
+    {"category": "Salads", "image":image7},
+    {"category": "Beverages", "image":image9},
+    {"category": "Soups", "image":image10},
+    {"category": "All", "image":image11},
+]
--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import Nav from './Nav'
+import React, { useRef } from "react";
+import Nav from "./Nav";
+import { categories } from "../category";
+import CategoryCard from "./CategoryCard";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaArrowCircleRight } from "react-icons/fa";
 
 const UserDashboard = () => {
+  const cateScrollRef = useRef()
   return (
-    <div className='w-full h-[80px] flex items-center justify-between md:justify-center gap-[30px] px-[20px] fixed top-0 z-[9999] bg-[#fff9f6] overflow-visible'>
-        <Nav/>
+    <div className="w-screen mih-h-screen flex flex-col gap-5 items-center bg-[#fff9f6] overflow-y-auto">
+      <Nav />
+      <div className="w-full max-w-6xl flex flex-col gap-5 items-start p-2.5">
+        <h1 className="text-gray-800 text-2xl sm:text-3xl">
+          What are you craving today?
+        </h1>
+        <div className="w-full relative">
+          <button className="absolute left-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 ">
+            <FaArrowCircleLeft />
+          </button>
+          <div className="w-full flex overflow-x-auto gap-4 pb-2" ref={cateScrollRef}>
+            {categories.map((cat, index) => (
+              <CategoryCard data={cat} key={index} />
+            ))}
+          </div>
+          <button className="absolute right-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 ">
+            <FaArrowCircleRight />
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserDashboard
+export default UserDashboard;
