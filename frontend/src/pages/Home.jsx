20e4b41af1d5c22806ce72d5ca7a6056415fd53e
pratham-jdn <2003prathamkumar@gmail.com>
2026-02-08 22:19:17 +0530

    fixed styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ import DeliveryBoy from '../components/DeliveryBoy'
 const Home = () => {
   const {userData} = useSelector(state=>state.user)
   return (
-    <div className='w-[100vw] min-h-[100vh] pt-[100px] flex flex-col items-center bg-[#fff9f6]'>
+    <div className='w-screen min-h-screen pt-25 flex flex-col items-center bg-[#fff9f6]'>
       {userData.role=="user" && <UserDashboard/>}
       {userData.role=="owner" && <OwnerDashboard/>}
       {userData.role=="deliveryBoy" && <DeliveryBoy/>}
